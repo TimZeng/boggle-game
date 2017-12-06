a340adb97798afd99938faba1c3b6782da528539
Tim Zeng <zengtiantian1122@gmail.com>
2017-12-06 15:55:26 -0800

    complete the UIs

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,45 @@
 import React, { Component } from 'react';
 
+const data = ['A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A','A'];
+
 class App extends Component {
   render() {
     return (
-      <h1>Hello World</h1>
+      <div className='container'>
+        <header>
+          <img src='images/logo.png'/>
+        </header>
+
+        <section>
+          <div id='board'>
+            {
+              data.map(el => <div className='dice'>{el}</div>)
+            }
+          </div>
+        </section>
+
+        <section>
+          <div id='tracking'>
+            <span><strong>Current Word</strong>: MAW</span>
+            <button>Submit Word</button>
+          </div>
+        </section>
+
+        <section>
+          <div id='score-table'>
+            <div id='scores'>
+              <div><strong>Word</strong></div>
+              <div><strong>Score</strong></div>
+              <div>congruent</div>
+              <div>11</div>
+              <div>urgent</div>
+              <div>3</div>
+              <div className='selected'><strong>Total:</strong></div>
+              <div className='selected'><strong>14</strong></div>
+            </div>
+          </div>
+        </section>
+      </div>
     )
   }
 };
